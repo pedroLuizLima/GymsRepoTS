306675f5e662dec36f5e166cedf0ccdadb922289
Pedro Luiz <pedroluizgo@gmail.com>
2025-06-10 17:06:22 -0300

    Adding authentication verify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -14,16 +14,23 @@ export async function authenticate (request: FastifyRequest, reply: FastifyReply
     try {
         const authenticateUseCase = MakeAuthenticateUseCase()
 
-        await authenticateUseCase.execute({
+        const { user } = await authenticateUseCase.execute({
             email,
             password
         })
+
+        const token = reply.jwtSign({}, {
+            sign: {
+                sub: user.id
+            }
+        })
+
+        return reply.status(200).send({ token })
+
     } catch (err) {
         if(err instanceof InvalidCredentialsError){
             return reply.status(400).send({ message: err.message })
         } 
         throw err
     }
-
-    return reply.status(200).send()
 }
