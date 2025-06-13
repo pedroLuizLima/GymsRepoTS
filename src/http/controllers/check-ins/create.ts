826651d846cf9350ff24307328b684cc062b39bd
Pedro Luiz <pedroluizgo@gmail.com>
2025-06-13 14:25:49 -0300

    Adding route to check-in

---
+++ src/http/controllers/check-ins/create.ts
@@ -0,0 +1,28 @@
+import { z } from "zod"
+import { FastifyRequest, FastifyReply} from 'fastify'
+import { MakeCheckInUseCase } from "../../../use-cases/factories/make-check-in-use-case"
+
+export async function create (request: FastifyRequest, reply: FastifyReply) {
+    const createCheckInParamsSchema = z.object({
+        gymId: z.string().uuid()
+    })
+    
+    const createCheckInBodySchema = z.object({
+        latitude: z.number().refine(value => { return Math.abs(value) <= 90 }),
+        longitude: z.number().refine(value => { return Math.abs(value) <= 180 })
+    })
+
+    const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
+    const { gymId } = createCheckInParamsSchema.parse(request.params)
+
+        const checkInUseCase = MakeCheckInUseCase()
+
+        await checkInUseCase.execute({
+            gymId,
+            userId: request.user.sub,
+            userLatitude: latitude,
+            userLongitude: longitude
+        })
+
+    return reply.status(201).send()
+}
