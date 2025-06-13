310587dd3d5bf34525641d260f9347f0eea91022
Pedro Luiz <pedroluizgo@gmail.com>
2025-06-13 14:01:23 -0300

    Adding route to create gym

---
+++ src/http/controllers/gyms/create.ts
@@ -0,0 +1,27 @@
+import { z } from "zod"
+import { FastifyRequest, FastifyReply} from 'fastify'
+import { UserAlredyExistsError } from "../../../use-cases/errors/user-alredy-exists-error"
+import { MakeCreateGymUseCase } from "../../../use-cases/factories/make-create-gym-use-case"
+
+export async function create (request: FastifyRequest, reply: FastifyReply) {
+    const createGymBodySchema = z.object({
+        title: z.string(),
+        description: z.string().nullable(),
+        phone: z.string().nullable(),
+        latitude: z.number().refine(value => { return Math.abs(value) <= 90 }),
+        longitude: z.number().refine(value => { return Math.abs(value) <= 180 })
+    })
+
+    const { title, description, phone, latitude, longitude } = createGymBodySchema.parse(request.body)
+        const createGymUseCase = MakeCreateGymUseCase()
+
+        await createGymUseCase.execute({
+            title,
+            description,
+            phone,
+            latitude,
+            longitude
+        })
+
+    return reply.status(201).send()
+}
