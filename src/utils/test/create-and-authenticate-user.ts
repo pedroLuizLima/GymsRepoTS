7bf949eed4f7690f224e0ff8f77eba3a4c82d84e
Pedro Luiz <pedroluizgo@gmail.com>
2025-06-16 16:00:52 -0300

    Adding role verify

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,12 +1,16 @@
 import { FastifyInstance } from "fastify"
 import request from "supertest"
+import { prisma } from "../../lib/prisma"
+import { hash } from "bcryptjs"
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-    await request(app.server).post('/users')
-    .send({
-        name: 'Pedro Luiz',
-        email: 'pedro123@example.com',
-        password: '123456',
+export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = false) {
+    await prisma.user.create({
+        data: {
+            name: 'Pedro Luiz',
+            email: 'pedro123@example.com',
+            password_hash: await hash('123456', 6),
+            role: isAdmin ? 'ADMIN' : 'MEMBER'
+        }
     })
 
     const authResponse = await request(app.server).post('/sessions')
