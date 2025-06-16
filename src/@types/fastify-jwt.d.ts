d8d51f106b02d55c6fc1f72ee52633bda0f482c7
Pedro Luiz <pedroluizgo@gmail.com>
2025-06-16 15:22:04 -0300

    Adding role to users

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -3,7 +3,8 @@ import "@fastify/jwt"
 declare module "@fastify/jwt" {
   export interface FastifyJWT {
     user: {
+      role: 'ADMIN' | 'MEMBER'
       sub: string
-    } // user type is return type of `request.user` object
+    }
   }
 }
